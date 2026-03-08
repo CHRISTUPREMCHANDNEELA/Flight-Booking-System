e23155ec1f2df03175c03830a8d90b7e9e7dddcb
CHRISTUPREMCHANDNEELA <neelachristupremchand@gmail.com>
2026-03-09 02:50:52 +0530

    added element sectors for booking details and buttons in delete.js

--- delete.js
+++ delete.js
@@ -1,10 +1,12 @@
-const bookingsForm = document.getElementById("bookingForm");
-const API_URL = "https://localhost:300/api/bookings";
-bookingForm.addEventListner("Submit",async(e) =>) {
-    e.preventDefault();
-    const booking = {
-        name: document.getElementById("name").ariaValueMax.trim(),
-    }
-    }
+const fetchBtn = document.getElementById("fetchBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const emailSearch = document.getElementById("emailSearch");
+const bookingDetails = document.getElementById("bookingDetails");
 
-}
+const nameSpan = document.getElementById("name");
+const genderSpan = document.getElementById("gender");
+const ageSpan = document.getElementById("age");
+const PhoneSpan = document.getElementById("phone");
+const journeyPlaceSpan = document.getElementById("journeyPlace");
+const journeyDateSpan = document.getElementById("journeyDate");
+
