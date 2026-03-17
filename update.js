395e93e9882ac2bf9600cf25387fea13c48d9565
CHRISTUPREMCHANDNEELA <neelachristupremchand@gmail.com>
2026-03-17 21:49:10 +0000

    setup booking form inputs and fetch button

---
+++ update.js
@@ -0,0 +1,12 @@
+const fetchBtn = document.getElementById("fetchBtn");
+const emailSearch = document.getElementById("emailSearch");
+const updateForm = document.getElementById("updateForm");
+
+const nameInput = document.getElementById("name");
+const genderInput = document.getElementById("gender");
+const ageInput = document.getElementById("age");
+const phoneInput = document.getElementById("phone");
+const journeyplaceInput = document.getElementById("journeyPlace");
+const journeyDateInput = document.getElementById("journeyPlace");
+
+let currentBookingId = null;
