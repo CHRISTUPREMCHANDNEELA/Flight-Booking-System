3a94d8c657e1e66c76621d4630817689f0422a6d
CHRISTUPREMCHANDNEELA <neelachristupremchand@gmail.com>
2026-03-20 21:28:22 +0000

    fetch syntax and email input handling

--- update.js
+++ update.js
@@ -10,3 +10,13 @@ const journeyplaceInput = document.getElementById("journeyPlace");
 const journeyDateInput = document.getElementById("journeyPlace");
 
 let currentBookingId = null;
+
+fetchBtn.addEventListener("click", async () => {
+    const email = emailSearch.ariaValueMax.trim();
+    if (!email) return alert("please enter your email");
+
+    try (
+        const res = await fetch(`http://localhost:3000/api/bookings/email/${email}`);
+        const data = await res.json();
+    )
+})
